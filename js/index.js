dc14b86a8e1bbf035a1a56617455235d5902670a
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-01-26 16:34:56 +0100

    solved loab

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+console.log("index.js Loaded");
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -24,3 +27,86 @@ const cards = [
   { name: 'the avengers', img: 'the-avengers.jpg' },
   { name: 'thor', img: 'thor.jpg' }
 ];
+
+let memoryGame = new MemoryGame(cards);
+
+const memoryBoard = document.getElementById("memory-board");
+
+const cardElements = memoryGame.cards.map(
+  card =>{
+
+    const outsideDiv = document.createElement("div");
+    
+    outsideDiv.classList.add("card")
+    outsideDiv.setAttribute("data-card-name",card.name)
+
+    const backDiv = document.createElement("div");
+    backDiv.classList.add("back");
+    backDiv.setAttribute("name", card.img);
+
+    const frontDiv = document.createElement("div")
+    frontDiv.classList.add("front")
+    frontDiv.style = `background: url(img/${card.img}) no-repeat`
+
+    outsideDiv.appendChild(backDiv);
+    outsideDiv.appendChild(frontDiv);
+
+    return outsideDiv;
+
+    
+      // These actions can only be taken when the page is completely loaded
+  /*
+   *
+   The following code creates a html element that has the following contents
+    <div class="card" data-card-name="${card.name}">
+      <div class="back" name="${card.img}"></div>
+      <div class="front" style="background: url(img/${card.img}) no-repeat"></div>
+    </div>
+  */
+
+  }  
+)
+
+cardElements.forEach(card=> {
+  
+  card.addEventListener("click",
+  event => {
+    const clickedCard = event.currentTarget;
+    flipCard(clickedCard);
+
+    const playResults = memoryGame.playCard(clickedCard)
+
+      // the following line is based on the fact that I would like to pass complex object from the game logic
+      // the object is: { isPair: false, playedCards: this.playedCards }
+      if(playResults.isPair){
+        playResults.playedCards.forEach(card=> setCardsToGuessed(card))
+      
+      }else{
+        playResults.playedCards.forEach( card=>{
+          setTimeout(()=>flipCard(card), 1 * 1000)
+        })
+      }
+        updateScoreBoard(
+          memoryGame.score,
+          memoryGame.clickedPairs,
+          memoryGame.guessedPairs
+          )
+          if(memoryGame.checkIfGameOver()) gameWon();      
+})
+  memoryBoard.appendChild(card);
+})
+
+const updateScoreBoard=(score,clicked,guessed)=>{
+  document.getElementById('score').innerText = score;
+  document.getElementById('pairs-clicked').innerText = clicked;
+  document.getElementById('pairs-guessed').innerText = guessed;
+}
+
+
+const flipCard = (card) => {
+  card.classList.toggle("turned");
+}
+
+function setCardsToGuessed(card) {
+  card.classList.add('guessed');
+}
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,60 @@
+console.log("memory.js Loaded");
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.guessedPairs === 0;
+
+    this.playedCards = [];
+
+    this.score = 0;
+    this.clickedPairs = 0;
+
+    this.shuffleCards();
   }
 
   shuffleCards() {
     // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 
-  checkIfPair(card1, card2) {
+  checkIfPair() {
     // ... write your code here
+    console.log(this.playedCards)
+    if(this.playedCards[0].dataset.cardName ===this.playedCards[1].dataset.cardName)
+    return true;
+
   }
 
-  checkIfFinished() {
+  checkIfGameOver() {
     // ... write your code here
+   return this.guessedPairs === this.cards.length / 2;
+  }
+
+  playCard(card){
+
+    let playResults ={ "isPair": false, "playedCards": [] }
+
+    if(this.playedCards.length < 2){
+      this.playedCards.push(card);
+    }
+    
+    if(this.playedCards.length === 2){
+      this.clickedPairs++;
+      if(this.checkIfPair()){
+        playResults = { isPair: true, playedCards: this.playedCards }
+        this.score++;
+        this.guessedPairs++;
+        this.playedCards = [];
+      }else{
+        playResults = { isPair: false, playedCards: this.playedCards }
+        this.playedCards = [];
+      };
+    }
+    return playResults; 
   }
 }
 
